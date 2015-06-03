6efad61966617e3ac5054bdab4669bb7cf50d581
Raghu Reddy <raghureddy.ram@gmail.com>
2015-06-02 17:44:15 -0700

    Fix routing

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,17 +1,4 @@
 get '/' do
-  erb :index
-end
-
-get '/things' do
-  if request.xhr?
-    resp = Unirest.get "https://api.forecast.io/forecast/81c88efeafdeb098caa777f0281510a8/37.8267,-122.423"
-    resp.body.to_json
-  else
-    status 404
-  end
-end
-
-
-get '/landing' do
   erb :landing
 end
+
