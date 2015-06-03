506eeb5fa34fac168ae6d13ee1cb9321742d2c1b
Raghu Reddy <raghureddy.ram@gmail.com>
2015-06-03 15:45:29 -0700

    add click through functionality for graph transition

--- public/js/call_graphs.js
+++ public/js/call_graphs.js
@@ -7,23 +7,33 @@ $(document).ready(function(){
   $('.call-graph-button').on('click', '.solid-darkgreen', function(){
     console.log('test');
     d3.selectAll("svg > *").remove()
-    lineCharts.compareCities(cities.retrieve('from'), cities.retrieve('to'), '#feels_like_graph', feelsLikeCity['params'][0]);
+    lineCharts.singleCity(cities.retrieve('to'),'#feels_like_graph', feelsLikeCity);
   });
 
+  chartArray = feelsLikeCity['params'];
+  var feelsLikeIndex = 1;
+
   $('#panel1').on('click', '.transition-button', function() {
-    d3.selectAll("svg > *").remove()
-    var chartArray =
-    [
-    lineCharts.compareCities(cities.retrieve('from'), cities.retrieve('to'), '#feels_like_graph', feelsLikeCity['params'][1]),
-    lineCharts.compareCities(cities.retrieve('from'), cities.retrieve('to'), '#feels_like_graph', feelsLikeCity['params'][2]),
-    lineCharts.compareCities(cities.retrieve('from'), cities.retrieve('to'), '#feels_like_graph', feelsLikeCity['params'][3]),
-    lineCharts.singleCity(cities.retrieve('to'),'#feels_like_graph', feelsLikeCity),
-    lineCharts.singleCity(cities.retrieve('from'),'#feels_like_graph', feelsLikeCity)
-    ]
-    for(i=0; i<chartArray.length; i++){
-      chartArray[i];
-    }
+    d3.selectAll("svg > *").remove();
+    lineCharts.compareCities(cities.retrieve('from'), cities.retrieve('to'), '#feels_like_graph', feelsLikeCity['params'][feelsLikeIndex]);
+    feelsLikeIndex = (feelsLikeIndex + 1) % (chartArray.length);
+    ;
   })
+
+  // $('#panel1').on('click', '.transition-button', function() {
+  //   d3.selectAll("svg > *").remove();
+  //   lineCharts.compareCities(cities.retrieve('from'), cities.retrieve('to'), '#feels_like_graph', feelsLikeCity['params'][feelsLikeIndex]);
+  //   feelsLikeIndex = (feelsLikeIndex + 1) % (chartArray.length);
+  //   ;
+  // })
+
+  // function nextElement() {
+  // d3.selectAll("svg > *").remove()
+  //  feelsLikeIndex = (feelsLikeIndex+1)%(chartArray.length);
+  //  debugger
+  //  return chartArray[feelsLikeIndex];
+  // };
+
 
   $('.call-graph-button').on('click', '.solid-orange-2', function(){
     console.log('test');
