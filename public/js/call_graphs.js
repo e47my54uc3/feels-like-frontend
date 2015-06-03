d69a785115becfda52678e022afa959b6613506e
Raghu Reddy <raghureddy.ram@gmail.com>
2015-06-03 14:34:11 -0700

    add transition button functionality, need closure loop on click

--- public/js/call_graphs.js
+++ public/js/call_graphs.js
@@ -4,19 +4,33 @@ $(document).ready(function(){
     // debugger
   });
 
+  $('.call-graph-button').on('click', '.solid-darkgreen', function(){
+    console.log('test');
+    d3.selectAll("svg > *").remove()
+    lineCharts.compareCities(cities.retrieve('from'), cities.retrieve('to'), '#feels_like_graph', feelsLikeCity['params'][0]);
+  });
+
+  $('#panel1').on('click', '.transition-button', function() {
+    d3.selectAll("svg > *").remove()
+    var chartArray =
+    [
+    lineCharts.compareCities(cities.retrieve('from'), cities.retrieve('to'), '#feels_like_graph', feelsLikeCity['params'][1]),
+    lineCharts.compareCities(cities.retrieve('from'), cities.retrieve('to'), '#feels_like_graph', feelsLikeCity['params'][2]),
+    lineCharts.compareCities(cities.retrieve('from'), cities.retrieve('to'), '#feels_like_graph', feelsLikeCity['params'][3]),
+    lineCharts.singleCity(cities.retrieve('to'),'#feels_like_graph', feelsLikeCity),
+    lineCharts.singleCity(cities.retrieve('from'),'#feels_like_graph', feelsLikeCity)
+    ]
+    for(i=0; i<chartArray.length; i++){
+      chartArray[i];
+    }
+  })
+
   $('.call-graph-button').on('click', '.solid-orange-2', function(){
     console.log('test');
     // d3.select('svg').remove()
     // debugger
     // console.log($('#actual_graph').parent());
     lineCharts.singleCity(cities.retrieve('to'),'#actual_graph', actualCity);
-  });
-
-
-  $('.call-graph-button').on('click', '.solid-darkgreen', function(){
-    console.log('test');
-    d3.selectAll("svg > *").remove()
-    lineCharts.singleCity(cities.retrieve('to'),'#feels_like_graph', feelsLikeCity);
   });
 
   $('.call-graph-button').on('click', '.solid-blue', function(){
@@ -29,6 +43,22 @@ $(document).ready(function(){
     console.log('red');
     //insert calls for the daylight
   });
-  
 
+
+  $('#panel2').on('click', '.transition-button', function() {
+    d3.selectAll("svg > *").remove()
+    lineCharts.singleCity(cities.retrieve('from'),'#actual_graph', actualCity);
+  })
+
+  $('#panel3').on('click', '.transition-button', function() {
+    d3.selectAll("svg > *").remove()
+    lineCharts.singleCity(cities.retrieve('from'),'#rain_snow_graph', rainSnowCity);
+  })
+
+  $('#panel4').on('click', '.transition-button', function() {
+    d3.selectAll("svg > *").remove()
+    lineCharts.singleCity(cities.retrieve('from'),'#sunlight_graph', SunlightCity);
+  })
+  
 })
+
