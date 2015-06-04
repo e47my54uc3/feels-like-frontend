2ca7ef1b255d54405c047d92b5b5e6417129b46a
Raghu Reddy <raghureddy.ram@gmail.com>
2015-06-04 11:33:12 -0700

    fix require pry

--- config/environment.rb
+++ config/environment.rb
@@ -14,7 +14,7 @@ require 'unirest'
 require 'pg'
 require 'active_record'
 require 'logger'
-require 'pry'
+require 'pry' if development?
 require 'sinatra'
 require "sinatra/reloader" if development?
 
